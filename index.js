45920fdb5b792813a30b79b246c2464786541469
codeJaboo <jacobbranson07@gmail.com>
2026-04-16 15:33:24 +0300

    final

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
-// index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
+const input = document.getElementById("stateInput");
+const button = document.getElementById("getAlertsBtn");
+const results = document.getElementById("results");
+const errorBox = document.getElementById("error");
 
-// Your code here!
+button.addEventListener("click", function () {
+  let state = input.value;
+
+  input.value = "";
+
+  fetchWeatherData(state);
+});
+
+async function fetchWeatherData(state) {
+  try {
+    if (!state) {
+      displayError("Enter state first");
+      return;
+    }
+
+    let res = await fetch("https://api.weather.gov/alerts/active?area=" + state);
+
+    if (!res.ok) {
+      displayError("Something went wrong");
+      return;
+    }
+
+    let data = await res.json();
+
+    displayWeather(data);
+
+  } catch (err) {
+    displayError("Error loading data");
+  }
+}
+
+function displayWeather(data) {
+  let alerts = data.features;
+
+  results.innerHTML =
+    "Current watches, warnings, and advisories: " + alerts.length;
+
+  errorBox.innerHTML = "";
+}
+
+function displayError(msg) {
+  errorBox.innerHTML = msg;
+  results.innerHTML = "";
+}
